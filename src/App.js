2700e74cad7625fe74d6ad0f100ad123027368a4
muratozkan79 <muratozkan79@gmail.com>
2023-03-04 11:18:09 +0300

    started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Main from "./pages/Main";
+import NewProduct from "./pages/NewProduct";
+import ProductList from "./pages/ProductList";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
 
 function App() {
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-light app">
+      <Navbar/>
+      <Main/>
+      <About/>
+      <NewProduct/>
+      <ProductList/>
     </div>
   );
 }
